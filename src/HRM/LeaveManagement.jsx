4c7764d56602fc7d9546f4feaed7c78f0f192018
Muhammad Qasim <qasimmuhammad278@gmail.com>
2025-05-26 11:45:25 +0500

    Create Pages for payroll

---
+++ src/HRM/LeaveManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LeaveManagement() {
+  return (
+    <div><h1>Leave Management</h1></div>
+  )
+}
+
+export default LeaveManagement
---
+++ src/HRM/TaxSetup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TaxSetup() {
+  return (
+    <div><h1>Tax Setup</h1></div>
+  )
+}
+
+export default TaxSetup
